e2db8e9390ab8fd1cda263b18f9bedb1a2e9bddf
Marthenn <bintang.dwimarthen@gmail.com>
2022-12-10 00:02:49 +0700

    chore: change the graph to fit the graph in the paper

--- main.cpp
+++ main.cpp
@@ -70,13 +70,17 @@ void Graph::topologicalSort(){
 // main program
 int main(){
     // Buat Graph
-    Graph g(6);
-    g.addEdge(5, 2);
-    g.addEdge(5, 0);
-    g.addEdge(4, 0);
-    g.addEdge(4, 1);
-    g.addEdge(2, 3);
-    g.addEdge(3, 1);
+    Graph g(11);
+    g.addEdge(2,7);
+    g.addEdge(6,7);
+    g.addEdge(1,7);
+    g.addEdge(3,8);
+    g.addEdge(7,8);
+    g.addEdge(0,9);
+    g.addEdge(8,9);
+    g.addEdge(5,9);
+    g.addEdge(4,10);
+    g.addEdge(9,10);
     cout<<"Topological Sort: \n";
     g.topologicalSort();
     return 0;
